69e4b2d9ecb2235ee76b64b0f581752e21c96343
Andrew Nemzorov <nemzorovandrew@mail.ru>
2023-04-15 20:25:39 +0300

    v12

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default='1234567890')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
